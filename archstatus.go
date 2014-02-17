b74a1f8e50ce6fd96a434c73787ada278a31b02a
Michael Prinsloo <mike.prinsloo@gmail.com>
2014-02-17 22:06:16 +0200

    Created a pipe between systemctl and grep.

--- archstatus.go
+++ archstatus.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
-	"log"
+	//"log"
+	//"os"
 )
 
 func main() {
-	out, e := exec.Command("date").Output()
-	if e != nil {
-		log.Fatal(e)
-	}
-	fmt.Printf("The date is %s\n", out)
+
+	// The systemctl command.
+	syscommand := exec.Command("systemctl", "status", "cronie.service")
+
+	// The grep command.
+	grepcommand := exec.Command("grep", "Active:")
+
+	// Pipe the stdout of syscommand to the stdin of grepcommand.
+	grepcommand.Stdin, _ = syscommand.StdoutPipe()
+
+	// Create a buffer of bytes.
+	var b bytes.Buffer
+
+	// Assign the address of our buffer to grepcommand.Stdout.
+	grepcommand.Stdout = &b
+
+	// Start grepcommand.
+	_ = grepcommand.Start()
+
+	// Run syscommand
+	_ = syscommand.Run()
+
+	// Wait for grepcommand to exit.
+	_ = grepcommand.Wait()
+
+	fmt.Printf("%s", &b)
 }
